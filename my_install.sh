b0ae18979b9690827d036b13e8b0a7149be28fe1
Stairxur <stairxur@gmail.com>
2024-07-27 03:45:43 +0300

    27.07.24-1

--- my_install.sh
+++ my_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing programs..."
-sudo pacman -S hyprland hyprpaper waybar firefox neovim alacritty mc btop git cmake smplayer cmus wofi evince transmission-gtk numlockx udiskie pamixer grim slurp zip unzip lib32-pipewire
+sudo pacman -S hyprland hyprpaper waybar firefox neovim alacritty mc btop cmake smplayer cmus wofi evince transmission-gtk numlockx udiskie pamixer grim slurp zip unzip telegram-desktop
 
 echo "Installing fonts..."
 sudo pacman -S ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ttf-hanazono ttf-jetbrains-mono-nerd
