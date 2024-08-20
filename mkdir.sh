543c4ea03236eded26cc5e7fb4b65dca477d4494
Stairxur <stairxur@gmail.com>
2024-08-20 21:46:57 +0300

    20.08.24-2

---
+++ mkdir.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Created folders..."
+mkdir ~/Desktop
+mkdir ~/Documents
+mkdir ~/Downloads
+mkdir ~/GitHub
+mkdir ~/Movies
+mkdir ~/Music
+mkdir ~/My\ Projects
+mkdir ~/Pictures
+mkdir ~/Pictures/Screenshots
+mkdir ~/Public
+mkdir ~/Templates
+mkdir ~/Videos
